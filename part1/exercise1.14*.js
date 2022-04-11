6c5e4bedca49bb33c138c0a81fc0fd41566a9f16
Roberto <101400330+r0b3m@users.noreply.github.com>
2022-04-11 19:09:58 +0200

    Create exercise1.14*.js

---
+++ part1/exercise1.14*.js
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+const [points, setPoints] = useState(new Uint8Array(6));  
+const [selected, setSelected] = useState(0)
+const [mostVoted, setMostVoted] = useState(0);
+const evenhandler = () =>{
+  setSelected(Math.floor(Math.random() * anecdotes.length))
+}
+const handleVoteClick = () => {
+  const copy = { ...points };
+  copy[selected] += 1;
+  setPoints(copy);
+  console.log('voted')
+};
+if (points[selected] > points[mostVoted]) {
+  setMostVoted(selected);
+}
+  return (
+    <div>
+            <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <br />
+      has {points[selected]} points
+      <br />
+        <button onClick={handleVoteClick}>Vote</button>
+        <button onClick={evenhandler}>Next anecdote</button>
+        <br />
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[mostVoted]}
+    </div>
+  );
+};
+export default App
